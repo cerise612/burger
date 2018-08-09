9b3745092a2b33bbb336cbb1d18e7de91baa6a40
Cerise <developerdanielle@gmail.com>
2018-08-09 09:57:32 -0700

    add

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,7 +7,8 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get("/api/", function(req, res) {
+  
     burger.all(function(data) {
       var hbsObject = {
         burgers: data
